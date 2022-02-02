c44c11c559ea8b33d23dedd7462cf696558c22e0
Carlos <94437617+CarlosMartin24@users.noreply.github.com>
2022-02-02 13:24:30 +0100

    Update app.js
    examen

--- app.js
+++ app.js
@@ -51,9 +51,20 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   ]
 
+  //EX 2-----------------------------------------
+  let tablero = document.querySelector(".grid")
+  let puntos = document.getElementById("result")
+
+  tablero.addEventListener('mouseout', () => {
+  puntos.style.fontSize = 20+"px"})
+    
+  tablero.addEventListener('mouseover', () => {
+  puntos.style.fontSize = 30+"px"})
+
   cardArray.sort(() => 0.5 - Math.random())
 
   const grid = document.querySelector('.grid')
+  
   const resultDisplay = document.querySelector('#result')
   let cardsChosen = []
   let cardsChosenId = []
@@ -67,19 +78,26 @@ document.addEventListener('DOMContentLoaded', () => {
       card.setAttribute('data-id', i)
       card.addEventListener('click', flipCard)
       grid.appendChild(card)
+      }
     }
-  }
-
+    //EX3--------------------------------------------
+    let contador = 4
   //check for matches
   function checkForMatch() {
     const cards = document.querySelectorAll('img')
     const optionOneId = cardsChosenId[0]
     const optionTwoId = cardsChosenId[1]
     
+    //EX3 cogemos las vidas
+    let vides = document.getElementById("vides")
+
     if(optionOneId == optionTwoId) {
       cards[optionOneId].setAttribute('src', 'images/blank.png')
       cards[optionTwoId].setAttribute('src', 'images/blank.png')
       alert('You have clicked the same image!')
+      //EX3 Vamos restando vidas al contador si clica las mismas imagenes
+      contador--
+      vides.innerHTML = contador
     }
     else if (cardsChosen[0] === cardsChosen[1]) {
       alert('You found a match')
@@ -92,7 +110,13 @@ document.addEventListener('DOMContentLoaded', () => {
       cards[optionOneId].setAttribute('src', 'images/blank.png')
       cards[optionTwoId].setAttribute('src', 'images/blank.png')
       alert('Sorry, try again')
+      //EX3 Restamos al contador si se equivoca
+      contador--
+      vides.innerHTML = contador
     }
+    //EX3 Mensaje de alerta cuando ya hemos perdido (contador==0) 
+    if (contador == 0) alert("Has perdut Carlos Martin Panzardo Becerra!")
+
     cardsChosen = []
     cardsChosenId = []
     resultDisplay.textContent = cardsWon.length
@@ -101,6 +125,13 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  //EX4 Hacemos que nos muestre el historial al hacer click
+  let accions = document.getElementById("accions")
+  let botonHistorial = document.getElementById("botonHistorial") 
+  botonHistorial.addEventListener('click', () => {
+    accions.style.display = "block"
+  })
+  let contadorAccions = 0
   //flip your card
   function flipCard() {
     let cardId = this.getAttribute('data-id')
@@ -110,7 +141,15 @@ document.addEventListener('DOMContentLoaded', () => {
     if (cardsChosen.length ===2) {
       setTimeout(checkForMatch, 500)
     }
+    contadorAccions++
+    
+    const taula = document.createElement('div')
+    const taulacontent = `
+    <p>Acció ${contadorAccions}: ${cardArray[cardId].name}</p>
+    `
+    taula.innerHTML = taulacontent
+    accions.append(taula)
+    console.log(taula);
   }
-
   createBoard()
 })
